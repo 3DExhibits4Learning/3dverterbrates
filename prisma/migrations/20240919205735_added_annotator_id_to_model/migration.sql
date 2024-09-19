ad36ec8cdcc4455e23e372fd7294ecc0b60b2c07
ajhsu415 <ab632@humboldt.edu>
2024-09-19 15:46:48 -0700

    Migrated schema change and changed env variable reference for laptop install

---
+++ prisma/migrations/20240919205735_added_annotator_id_to_model/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `model` ADD COLUMN `annotatorId` VARCHAR(50) NULL;
--- src/app/api/collections/models/route.tsx
+++ src/app/api/collections/models/route.tsx
@@ -4,7 +4,7 @@ import { getAllSiteReadyModels } from "@/api/queries"
 export async function GET(request: Request) {
 
       try{
-        const models = await getAllSiteReadyModels(!!(process.env.NEXT_PUBLIC_NODE_ENV === 'development'))
+        const models = await getAllSiteReadyModels(!!(process.env.NODE_ENV === 'development'))
         return Response.json({data: 'models got', response: models})
       }
       catch(e: any) {return Response.json({data:'Error getting models', response: e.message}, {status: 400, statusText: 'Error getting models'})}
