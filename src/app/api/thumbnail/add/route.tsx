c5634149fd5ad3a47e08c829f4808a7c4e545ddf
AJ <ab632@humboldt.edu>
2024-12-04 13:01:48 -0800

    route handler refactor

--- src/app/api/thumbnail/add/route.tsx
+++ src/app/api/thumbnail/add/route.tsx
@@ -11,6 +11,9 @@ import { updateThumbUrl } from '@/functions/server/queries'
 import { routeHandlerTypicalCatch, routeHandlerErrorHandler } from '@/functions/server/error'
 import routeHandlerTypicalResponse from '@/functions/server/typicalSuccessResponse'
 
+// Route
+const route = 'src/app/api/thumbnail/add/route.tsx'
+
 /**
  * 
  * @param request HTTP request
@@ -20,41 +23,37 @@ export async function POST(request: Request) {
 
     try {
 
-        // Route
-        const route = 'src/app/api/thumbnail/add/route.tsx'
-
         // Get form data and variables
-        const formData = await request.formData().catch(e => routeHandlerErrorHandler(route, e.message, 'formData()', "Form data missing" )) as FormData
+        const formData = await request.formData().catch(e => routeHandlerErrorHandler(route, e.message, 'formData()', "Form data missing")) as FormData
 
+        // Variable declarations
         const file = formData.get('file') as File
         const uid = formData.get('uid') as string
+        var path = `public/data/Vertebrates/Thumbnails/${uid}`
 
         // Return if any data is missing
         if (!file || !uid) throw Error('File or UID is missing')
 
-        // Convert photo to buffer and write to data storage
+        // file => arrayBuffer => Buffer
         const bytes = await file.arrayBuffer()
         const buffer = Buffer.from(bytes)
-        var path = `public/data/Vertebrates/Thumbnails/${uid}`
 
-        await mkdir(path, { recursive: true }).catch(e => routeHandlerErrorHandler(route, e.message, 'mkdir()', "Couldn't make directory" ))
+        // make directory
+        await mkdir(path, { recursive: true }).catch(e => routeHandlerErrorHandler(route, e.message, 'mkdir()', "Couldn't make directory"))
 
+        // file path
         path = join(path, file.name)
 
         //@ts-ignore - typescript thinks writeFile doesn't take a buffer
-        await writeFile(path, buffer).catch(e => routeHandlerErrorHandler(route, e.message, 'writeFile()', "Couldn't write file" ))
+        await writeFile(ath, buffer).catch(e => routeHandlerErrorHandler(route, e.message, 'writeFile()', "Couldn't write file"))
 
         // Update the thumbnail column for the model in the database (remove 'public' and following slash, then escape remaining forward slashes in path before DB entry)
-        const update = await updateThumbUrl(path.slice(7).replace('/', '\/'), uid).catch((e) => {
-            if (process.env.LOCAL_ENV === 'development') console.error(e.message)
-            throw Error("Couldn't update database")
-        })
+        const update = await updateThumbUrl(path.slice(7).replace('/', '\/'), uid).catch(e => routeHandlerErrorHandler(route, e.message, 'updateThumbUrl()', "Couldn't update DB record"))
 
-        //Return Successful
-        return Response.json({ data: "Thumbnail added", response: update })
+        // Typical response
+        return routeHandlerTypicalResponse("Thumbnail added", update)
     }
-    catch (e: any) {
-        if (process.env.LOCAL_ENV === 'development') console.error(e.message)
-        return Response.json({ data: 'Error Adding Thumbnail', response: 'Error Adding Thumbnail' }, { status: 400, statusText: 'Error Adding Thumbnail' })
-    }
+
+    // Typical catch
+    catch (e: any) { return routeHandlerTypicalCatch(e.message) }
 }
--- src/app/api/thumbnail/update/route.tsx
+++ src/app/api/thumbnail/update/route.tsx
@@ -1,66 +1,65 @@
+/**
+ * @file src/app/api/thumbnail/update/route.tsx
+ * 
+ * @fileoverview route handler for updating thumbnails
+ */
+
+// Imports
 import { writeFile, mkdir, unlink } from 'fs/promises'
 import { join } from 'path'
 import { updateThumbUrl, getModelThumbnail } from '@/functions/server/queries'
+import { routeHandlerTypicalCatch, routeHandlerErrorHandler } from '@/functions/server/error'
+import routeHandlerTypicalResponse from '@/functions/server/typicalSuccessResponse'
 
+// Route
+const route = 'src/app/api/thumbnail/update/route.tsx'
+
+/**
+ * 
+ * @param request HTTP request
+ * @returns typical response with update json for context
+ */
 export async function POST(request: Request) {
 
     try {
 
         // Get form data and variables
-        const formData = await request.formData().catch((e) => {
-            if (process.env.LOCAL_ENV === 'development') console.error(e.message)
-            throw Error("No Form Data")
-        })
+        const formData = await request.formData().catch(e => routeHandlerErrorHandler(route, e.message, 'formData()', "Form data missing")) as FormData
 
+        // Variable declarations
         const file = formData.get('file') as File
         const uid = formData.get('uid') as string
+        var path = `public/data/Vertebrates/Thumbnails/${uid}`
 
         // Get old thumbnail path
-        const oldThumbnailObject = await getModelThumbnail(uid).catch((e) => {
-            if (process.env.LOCAL_ENV === 'development') console.error(e.message)
-            throw Error("Couldn't find previous thumbnail")
-        })
+        const oldThumbnailObject = await getModelThumbnail(uid).catch(e => routeHandlerErrorHandler(route, e.message, 'getModelThumbnail()', "Couldn't get model thumbnail"))
 
         // Return if any data is missing
         if (!file || !uid) throw Error('File or UID is missing')
 
-        // Convert photo to buffer and write to data storage
+        // file => arrayBuffer => Buffer
         const bytes = await file.arrayBuffer()
         const buffer = Buffer.from(bytes)
-        var path = `public/data/Vertebrates/Thumbnails/${uid}`
 
         // Since this is an update, this path should already exist; this is a low-cost redundency check
-        await mkdir(path, { recursive: true }).catch((e) => {
-            if (process.env.LOCAL_ENV === 'development') console.error(e.message)
-            throw Error("Couldn't create directory")
-        })
+        await mkdir(path, { recursive: true }).catch(e => routeHandlerErrorHandler(route, e.message, 'mkdir()', "Couldn't make directory"))
 
         // Write file to path
         path = join(path, file.name)
 
         //@ts-ignore - typescript thinks writeFile doesn't take a buffer
-        await writeFile(path, buffer).catch((e) => {
-            if (process.env.LOCAL_ENV === 'development') console.error(e.message)
-            throw Error("Couldn't write file")
-        })
+        await writeFile(path, buffer).catch(e => routeHandlerErrorHandler(route, e.message, 'writeFile()', "Couldn't write file"))
 
         // Update the thumbnail column for the model in the database
-        const update = await updateThumbUrl(path.slice(7), uid).catch((e) => {
-            if (process.env.LOCAL_ENV === 'development') console.error(e.message)
-            throw Error("Couldn't update database")
-        })
+        const update = await updateThumbUrl(path.slice(7), uid).catch(e => routeHandlerErrorHandler(route, e.message, 'updateThumbUrl()', "Couldn't update DB record"))
 
         // Delete old thumbnail
-        await unlink('public/' + oldThumbnailObject?.thumbnail).catch((e) => {
-            if (process.env.LOCAL_ENV === 'development') console.error(e.message)
-            throw Error("Couldn't delete previous thumbnail")
-        })
+        await unlink('public/' + oldThumbnailObject?.thumbnail).catch(e => routeHandlerErrorHandler(route, e.message, 'unlink()', "Couldn't delete old thumbnail"))
 
-        //Return Successful
-        return Response.json({ data: "Thumbnail Updated", response: update })
+        // Typical response
+        return routeHandlerTypicalResponse("Thumbnail Updated", update)
     }
-    catch (e: any) {
-        if (process.env.LOCAL_ENV === 'development') console.error(e.message)
-        return Response.json({ data: 'Error Adding Thumbnail', response: 'Error Adding Thumbnail' }, { status: 400, statusText: 'Error Adding Thumbnail' })
-    }
+
+    // Typical catch
+    catch (e: any) { return routeHandlerTypicalCatch(e.message) }
 }
